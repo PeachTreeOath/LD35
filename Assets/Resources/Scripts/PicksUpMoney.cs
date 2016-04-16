0a54fa6280958c0da9a71e3c2d68043e31cd75af
mjeffery <michael.jeffery@gmail.com>
2016-04-16 11:53:41 -0400

    bank and money grabbing

---
+++ Assets/Resources/Scripts/PicksUpMoney.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PicksUpMoney : MonoBehaviour {
+
+    private Bank bank;
+
+	void Start () {
+        bank = GameObject.Find("Singletons").GetComponent<Bank>();
+    }
+
+    void onCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Money")
+        {
+            Money money = collisionInfo.collider.gameObject.GetComponent<Money>();
+            if (money != null)
+            {
+                bank.money += money.value;
+            }
+        }
+    }
+}
