7ae7cb306eaa17ef9c87908ec2db68400af5a820
Gary Wong <gawong@microsoft.com>
2016-04-16 12:16:08 -0700

    Add options and shop

---
+++ Assets/Menus/Scripts/Options.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Options : MonoBehaviour {
+    private CanvasGroup optionsCanvas;
+    // Use this for initialization
+    void Start () {
+        optionsCanvas = GameObject.Find("Options Screen").GetComponent<CanvasGroup>();
+        HideOptions();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ShowOptions()
+    {
+        optionsCanvas.alpha = 1;
+        optionsCanvas.blocksRaycasts = true;
+        optionsCanvas.interactable = true;
+    }
+
+    public void HideOptions()
+    {
+        optionsCanvas.alpha = 0;
+        optionsCanvas.blocksRaycasts = false;
+        optionsCanvas.interactable = false;
+    }
+}
