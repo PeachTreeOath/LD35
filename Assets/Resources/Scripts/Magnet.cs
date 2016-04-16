8f78e99622533a64fcb3d825394c35750212f44b
mjeffery <michael.jeffery@gmail.com>
2016-04-16 14:45:55 -0400

    add magnet to player

---
+++ Assets/Resources/Scripts/Magnet.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Magnet : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Magnetized magnetized = collider.gameObject.GetComponent<Magnetized>();
+        magnetized.magnet = gameObject;
+    }
+
+    protected void ResizeRadius()
+    {
+        CircleCollider2D collider = gameObject.GetComponent<CircleCollider2D>();
+        collider.radius = 2; //TODO make this work the way its supposed to...
+    }
+}
---
+++ Assets/Resources/Scripts/Magnetized.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Magnetized : MonoBehaviour {
+
+    public GameObject magnet;
+
+    public float stiffness = 100f;
+    public float damping = 50f;
+
+	void LateUpdate () {
+        if (magnet == null) return;
+
+        Rigidbody2D myBody = gameObject.GetComponent<Rigidbody2D>();
+        Rigidbody2D magnetBody = magnet.GetComponent<Rigidbody2D>();
+
+        if(myBody != null && magnetBody != null) 
+        {
+            Vector2 delta = magnetBody.position - myBody.position;
+            Vector2 acceleration = delta * stiffness - myBody.velocity * damping;
+
+            myBody.velocity += acceleration * Time.deltaTime;
+        }
+    }
+}
