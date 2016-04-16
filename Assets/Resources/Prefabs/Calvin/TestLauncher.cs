2d743f45fe0cebda88fced663cb9fae78dd982d7
PeachTreeOath <peachtreeoath@gmail.com>
2016-04-16 00:53:12 -0600

    Basic launch

---
+++ Assets/Resources/Prefabs/Calvin/TestLauncher.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestLauncher : MonoBehaviour
+{
+	public float force;
+	public GameObject playerPrefab;
+	private bool isShot;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetButtonDown ("Fire1") && !isShot) {
+			GameObject obj = (GameObject)Instantiate (playerPrefab, transform.position, Quaternion.identity);
+			Player player = obj.GetComponent<Player> ();
+			player.Init ();
+			player.Fire (transform.rotation.eulerAngles.z, force);
+
+			isShot = true;
+		}
+	}
+}
---
+++ Assets/Resources/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float groundYValue;
+	private Camera cam;
+	private Vector3 locationFromCam;
+	private Rigidbody2D body;
+
+	public void Init()
+	{
+		cam = GameObject.Find ("Main Camera").GetComponent<Camera> ();
+		locationFromCam = cam.transform.position - transform.position;
+		body = GetComponent<Rigidbody2D> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y < groundYValue) {
+			body.velocity = new Vector2(body.velocity.x, -body.velocity.y);
+		}
+	}
+
+	void LateUpdate()
+	{
+		cam.transform.position = transform.position + locationFromCam;
+	}
+
+	public void Fire(float angle, float force)
+	{
+		transform.Rotate (new Vector3(0,0,angle));
+		body.AddForce (new Vector2(Mathf.Cos (angle) * force, Mathf.Sin (angle) * force));
+	}
+}
