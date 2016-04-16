4d2f96c256a03ba0fa70a606d48dbe1049a9ae01
Gary Wong <gawong@microsoft.com>
2016-04-16 14:38:45 -0700

    Add shop panels for inventory and equipment

---
+++ Assets/Menus/Scripts/PurchaseAvatar.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PurchaseAvatar : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Purchase(string avatar)
+    {
+        /* if(currency >= cost)
+        {
+            // TODO: decrease currency by avatar cost
+            // TODO: increase avatar inventory in singleton
+        }
+        */
+    }
+}
