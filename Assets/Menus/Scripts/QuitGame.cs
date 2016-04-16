745f4d40a0db0cfaeb3a1b01765d20f30997cbc0
Gary Wong <gawong@microsoft.com>
2016-04-16 01:43:55 -0700

    Add quit functionality

---
+++ Assets/Menus/Scripts/QuitGame.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Quit()
+	{
+		//If we are running in a standalone build of the game
+		#if UNITY_STANDALONE
+		//Quit the application
+		Application.Quit();
+		#endif
+
+		//If we are running in the editor
+		#if UNITY_EDITOR
+		//Stop playing the scene
+		UnityEditor.EditorApplication.isPlaying = false;
+		#endif
+	}
+}
