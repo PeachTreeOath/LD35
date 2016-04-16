8d11a26e8faf7763b9c98eece1b059349210ee42
PeachTreeOath <peachtreeoath@gmail.com>
2016-04-16 01:02:35 -0600

    Fail stopping of ball

--- Assets/Resources/Scripts/Player.cs
+++ Assets/Resources/Scripts/Player.cs
@@ -1,14 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
-
+public class Player : MonoBehaviour
+{
+	// Determines ground level to bounce off of
 	public float groundYValue;
+	// Once player velocity is lower than this number, stop gameplay
+	public float minVelocityValue;
+
 	private Camera cam;
 	private Vector3 locationFromCam;
 	private Rigidbody2D body;
 
-	public void Init()
+	public void Init ()
 	{
 		cam = GameObject.Find ("Main Camera").GetComponent<Camera> ();
 		locationFromCam = cam.transform.position - transform.position;
@@ -16,25 +20,41 @@ public class Player : MonoBehaviour {
 	}
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if (transform.position.y < groundYValue) {
-			body.velocity = new Vector2(body.velocity.x, -body.velocity.y);
+	void Update ()
+	{
+		Debug.Log (body.velocity.magnitude);
+		// Don't bounce if velocity is too small
+		if (body.velocity.magnitude > minVelocityValue) {
+			if (transform.position.y < groundYValue) {
+				Bounce ();	
+			}	
 		}
 	}
 
-	void LateUpdate()
+	void LateUpdate ()
 	{
 		cam.transform.position = transform.position + locationFromCam;
 	}
 
-	public void Fire(float angle, float force)
+	public void Fire (float angle, float force)
 	{
-		transform.Rotate (new Vector3(0,0,angle));
-		body.AddForce (new Vector2(Mathf.Cos (angle) * force, Mathf.Sin (angle) * force));
+		transform.Rotate (new Vector3 (0, 0, angle));
+		body.AddForce (new Vector2 (Mathf.Cos (angle) * force, Mathf.Sin (angle) * force));
+	}
+
+	private void Bounce ()
+	{
+		body.velocity = new Vector2 (body.velocity.x, -body.velocity.y);
+	}
+
+	private void Stop ()
+	{
+		body.velocity = new Vector2 (body.velocity.x, -body.velocity.y);
 	}
 }
