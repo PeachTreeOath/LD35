dae3624f928f8cd0f0c8c9d33d7d05e5b5c48c9c
Gary Wong <gawong@microsoft.com>
2016-04-16 18:20:46 -0700

    Add upgrade amounts

--- Assets/Menus/Scripts/PurchaseAvatar.cs
+++ Assets/Menus/Scripts/PurchaseAvatar.cs
@@ -4,32 +4,37 @@ using System.Collections;
 
 public class PurchaseAvatar : MonoBehaviour {
     public int cost;
+    public int amount;
     private Text costText;
-    // Use this for initialization
+    private Text amountText;
+    private Bank bank;
+    private Inventory inventory;
+
     void Start () {
         cost = 1;
+        amount = 0;
         costText = transform.Find("Cost").GetComponent<Text>();
+        amountText = transform.Find("Amount").GetComponent<Text>();
+        inventory = GameObject.Find("Singletons").GetComponent<Inventory>();
+        bank = GameObject.Find("Singletons").GetComponent<Bank>();
     }
 	
-	// Update is called once per frame
 	void LateUpdate () {
         costText.text = string.Format(@"{0}", cost);
+        amountText.text = string.Format(@"{0}", amount);
 	}
 
     public void Purchase(string avatarString)
     {
         VishnuStateController.Avatar avatarEnum;
-
         avatarEnum = Utilities.EnumUtils<VishnuStateController.Avatar>.StringToEnum(avatarString);
-
-        Bank bank = GameObject.Find("Singletons").GetComponent<Bank>();
-        Inventory inventory = GameObject.Find("Singletons").GetComponent<Inventory>();
 
         if (bank.TotalMoney > cost)
         {
             cost += inventory.GetAvatarInventory(avatarEnum)+1;
             bank.Subtract(cost);
             inventory.IncrementAvatar(avatarEnum);
+            amount = inventory.GetAvatarInventory(avatarEnum);
         }
     }
 }
