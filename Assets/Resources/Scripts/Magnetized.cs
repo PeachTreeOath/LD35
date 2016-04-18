03175090aecd145037fd5e98d89d75a53d33b5ef
mjeffery <michael.jeffery@gmail.com>
2016-04-17 23:15:51 -0400

    change magnet speed

--- Assets/Resources/Scripts/Magnetized.cs
+++ Assets/Resources/Scripts/Magnetized.cs
@@ -7,7 +7,7 @@ public class Magnetized : MonoBehaviour {
 
     public float stiffness = 100f;
     public float damping = 25f;
-    public float maxSpeed = 30f;
+    public float maxSpeed = 15f;
 
 	void LateUpdate () {
         if (magnet == null) return;
