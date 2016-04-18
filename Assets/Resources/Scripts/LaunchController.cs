1c48b6456e16f439476fc06caeda8da9bc933079
PeachTreeOath <peachtreeoath@gmail.com>
2016-04-18 17:06:57 -0600

    Launch adheres to delta time

--- Assets/Resources/Scripts/LaunchController.cs
+++ Assets/Resources/Scripts/LaunchController.cs
@@ -2,146 +2,124 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class LaunchController : MonoBehaviour {
-    private Text angleText;
-    private Text powerText;
-    private Launcher launcher;
+public class LaunchController : MonoBehaviour
+{
+	private Text angleText;
+	private Text powerText;
+	private Launcher launcher;
 
-	private SpriteRenderer powerCircle; 
-	private SpriteRenderer arrow; 
+	private SpriteRenderer powerCircle;
+	private SpriteRenderer arrow;
 
-    private const float MAX_ANGLE = 90f;
-    private const float MIN_ANGLE = 0f;
+	private const float MAX_ANGLE = 90f;
+	private const float MIN_ANGLE = 0f;
 
-    private const float MAX_POWER = 100;
-    private const float MIN_POWER = 0;
+	private const float MAX_POWER = 100;
+	private const float MIN_POWER = 0;
 
-    private bool isAngleSet = false;
-    private bool isPowerSet = false;
+	private bool isAngleSet = false;
+	private bool isPowerSet = false;
 
-    private int angleDisplayed = 0;
-    private int powerDisplayed = 0;
+	private float angle;
+	private float power;
+	private float angleSpeed = 100f;
+	private float powerSpeed = 100f;
 
-    private int angleSelected = 0;
+	//delay between ticks of the spinners
+	public float spinnerDelayAngle = .01f;
+	public float spinnerDelayPower = .01f;
 
-    //delay between ticks of the spinners
-    public float spinnerDelayAngle = .01f;
-    public float spinnerDelayPower = .01f;
+	//True for increasing, false for decreasing
+	private bool isSpinnerIncreasing = true;
 
-    //True for increasing, false for decreasing
-    private bool isSpinnerIncreasing = true;
+	private float lastTick;
 
-    private float lastTick;
+	void Start ()
+	{
+		GameObject canvas = GameObject.Find ("Canvas");
+		launcher = GameObject.Find ("Launcher").GetComponent<Launcher> ();
 
-	void Start () {
-        GameObject canvas = GameObject.Find("Canvas");
-        launcher = GameObject.Find("Launcher").GetComponent<Launcher>();
-
-        Text[] textValue = canvas.GetComponentsInChildren<Text>();
+		Text[] textValue = canvas.GetComponentsInChildren<Text> ();
 		SpriteRenderer[] spriteValue = canvas.GetComponentsInChildren<SpriteRenderer> ();
-        angleText = textValue[1];
-        powerText = textValue[2];
+		angleText = textValue [1];
+		powerText = textValue [2];
         
-		powerCircle = spriteValue[1]; 
+		powerCircle = spriteValue [1]; 
 		powerCircle.enabled = false;
 		arrow = spriteValue [2]; 
 
 		lastTick = Time.time;
 
-        VishnuStateController.instance.PreFlight();
-    }
+		VishnuStateController.instance.PreFlight ();
+	}
 
-	void Update () {
-        //move the spinners
-        if (!isAngleSet)
-        {
-            if (Time.time >= lastTick + spinnerDelayAngle)
-            {
-                //First handle the edge cases
-                if (isSpinnerIncreasing && angleDisplayed >= MAX_ANGLE)
-                {
-                    //Flip the spinner
-                    isSpinnerIncreasing = false;
-					GameController.instance.PlaySound ("meter");
-                }
-                else if (!isSpinnerIncreasing && angleDisplayed <= MIN_ANGLE)
-                {
-                    isSpinnerIncreasing = true;
-					GameController.instance.PlaySound ("meter");
-                }
+	void Update ()
+	{
+		//move the spinners
+		if (!isAngleSet) {
+			//First handle the edge cases
+			if (isSpinnerIncreasing && angle >= MAX_ANGLE) {
+				//Flip the spinner
+				isSpinnerIncreasing = false;
+				GameController.instance.PlaySound ("meter");
+			} else if (!isSpinnerIncreasing && angle <= MIN_ANGLE) {
+				isSpinnerIncreasing = true;
+				GameController.instance.PlaySound ("meter");
+			}
 
-                //Now the standard increment/decrement cases
-                if (isSpinnerIncreasing)
-                {
-                    angleDisplayed++;
-                    angleText.text = "Angle: " + angleDisplayed;
+			//Now the standard increment/decrement cases
+			if (isSpinnerIncreasing) {
+				angle += Time.deltaTime * angleSpeed;
+				angleText.text = "Angle: " + Mathf.RoundToInt (angle);
 
-                }
-                else 
-                {
-                    angleDisplayed--;
-                    angleText.text = "Angle: " + angleDisplayed;
-                }
-				arrow.transform.rotation =  Quaternion.Euler(new Vector3(
-					0, 0, angleDisplayed)); 
-                lastTick = Time.time;
+			} else {
+				angle -= Time.deltaTime * angleSpeed;
+				angleText.text = "Angle: " + Mathf.RoundToInt (angle);
+			}
+			arrow.transform.rotation = Quaternion.Euler (new Vector3 (
+				0, 0, angle)); 
+                     
+		} else if (!isPowerSet) {
+			//First handle the edge cases
+			if (isSpinnerIncreasing && power >= MAX_POWER) {
+				//Flip the spinner
+				isSpinnerIncreasing = false;
+				GameController.instance.PlaySound ("flame");
+			} else if (!isSpinnerIncreasing && power <= MIN_POWER) {
+				isSpinnerIncreasing = true;
+				GameController.instance.PlaySound ("flame");
+			}
 
-            }
-        } 
-        else if (!isPowerSet)
-        {
-            if (Time.time >= lastTick + spinnerDelayPower)
-            {
-                //First handle the edge cases
-                if (isSpinnerIncreasing && powerDisplayed >= MAX_POWER)
-                {
-                    //Flip the spinner
-                    isSpinnerIncreasing = false;
-					GameController.instance.PlaySound ("flame");
-                }
-                else if (!isSpinnerIncreasing && powerDisplayed <= MIN_POWER)
-                {
-                    isSpinnerIncreasing = true;
-					GameController.instance.PlaySound ("flame");
-                }
+			//Now the standard increment/decrement cases
+			if (isSpinnerIncreasing) {
+				power += Time.deltaTime * powerSpeed;
+				powerText.text = "Power: " + power;
+				float scale = power / 10;
+				powerCircle.transform.localScale = new Vector2 (scale, scale);
+			} else {
+				power -= Time.deltaTime * powerSpeed;
+				powerText.text = "Power: " + power;
+				float scale = power / 10;
+				powerCircle.transform.localScale = new Vector2 (scale, scale);
+			}
 
-                //Now the standard increment/decrement cases
-                if (isSpinnerIncreasing)
-                {
+		}
 
-                    powerDisplayed++;
-                    powerText.text = "Power: " + powerDisplayed;
-					powerCircle.transform.localScale += new Vector3 (.1f, .1f, 0f);
-                }
-                else
-                {
-                    powerDisplayed--;
-                    powerText.text = "Power: " + powerDisplayed;
-					powerCircle.transform.localScale -= new Vector3 (.1f, .1f, 0f);
-                }
-
-                lastTick = Time.time;
-            }
-        }
-
-        //handle inputs
-        if (Input.GetButtonDown("Fire1") && !isAngleSet)
-		{
-            angleSelected = angleDisplayed;
-            isAngleSet = true;
+		//handle inputs
+		if (Input.GetButtonDown ("Fire1") && !isAngleSet) {
+			//angleSelected = angle;
+			isAngleSet = true;
 			powerCircle.enabled = true;
 			GameController.instance.ShowTutorialPhase (Tutorial.Phase.POWER);
 			GameController.instance.PlaySound ("flame");
-        }
-        else if (Input.GetButtonDown("Fire1") && !isPowerSet)
-        {
-            //don't need to save the power here since we are going to launch immediately
-            isPowerSet = true;
-            launcher.LaunchPlayer(angleSelected, powerDisplayed);
-            VishnuStateController.instance.StartFlight();
+		} else if (Input.GetButtonDown ("Fire1") && !isPowerSet) {
+			//don't need to save the power here since we are going to launch immediately
+			isPowerSet = true;
+			launcher.LaunchPlayer (angle, power);
+			VishnuStateController.instance.StartFlight ();
 			GameController.instance.ShowTutorialPhase (Tutorial.Phase.SWITCH);
 			GameObject.Find ("VishnuStart").GetComponent<SpriteRenderer> ().enabled = false;
-        }
+		}
 
-    }
+	}
 }
