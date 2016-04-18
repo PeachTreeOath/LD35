d9a587c94364dcd7f94456b40e09c76d38aaa697
mjeffery <michael.jeffery@gmail.com>
2016-04-17 21:07:31 -0400

    tastiness in progress

--- Assets/Resources/Scripts/Tastiness.cs
+++ Assets/Resources/Scripts/Tastiness.cs
@@ -1,9 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Tastiness : Magnet {
+public class Tastiness : MonoBehaviour {
 
-	protected override void OnTriggerEnter2D(Collider2D collider) {
-        base.OnTriggerEnter2D(collider);
+    private float range;
+    private bool isPrey = false;
+
+    public float Value
+    {
+        get { return range; }
+        set
+        {
+            bool prevPrey = isPrey;
+
+            isPrey = value > 0;
+            range = value;
+
+            if (prevPrey != isPrey)
+                OnStateChange(isPrey, range);
+        }
+    }
+
+    void OnStateChange(bool isPrey, float range)
+    {
+        if (isPrey)
+            BecomeTheHunted(range);
+        else
+            BecomeTheHunter(range);
+    }
+
+    void BecomeTheHunted(float range) {
+
+    }
+
+    void BecomeTheHunter(float range) {
+
+    }
+
+    bool OnObstacleEnter(Collider2D collider)
+    {
+        return true;
     }
 }
